73666654597bd069f88b538f342f7510915cd0bf
Hong <hong.yu@groupe-esigelec.org>
2018-10-10 13:09:08 +0200

    update Unit4.R

--- exercise_R/Unit4.R
+++ exercise_R/Unit4.R
@@ -12,3 +12,25 @@ install.packages('TeachingDemos')
 library(TeachingDemos)
 z.test(income, sd=50)
 
+#confidence interval about the proportion
+prop.test(120,200, conf.level = 0.95)
+
+
+#example
+before <- c(26.2,20.3,25.4,19.6,21.5,28.3,23.7,24)
+after <-c(24.1,21.3,23.7,18,20.1,25.8,22.4,21.4)
+difference <- before-after
+t.test(difference)
+
+t.test(difference, conf.level = 0)
+
+#exemple
+speed <- c(69,60,80,85,68,74,60,86,92)
+t.test(speed, mu=70,alternative = "two.sides") #H1:not equal
+
+t.test(speed, mu=70, alternative = "less")
+t.test(speed, mu=70, alternative = "greater")
+
+#unites produces x:defective
+prop.test(2,20,p=0.05,alternative = "two.sided")
+prop.test(20,200,p=0.05,alternative = "two.sided")
