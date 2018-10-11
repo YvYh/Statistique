1fded78a2c7e0ee24ee4fb4c03e73db222378da5
Hong <hong.yu@groupe-esigelec.org>
2018-10-11 10:30:40 +0200

    two-facotrs

--- exercise_R/Unit5.R
+++ exercise_R/Unit5.R
@@ -13,3 +13,36 @@ require(graphics)
 plot(TukeyHSD(anova_resources,"Resource_Factor",ordered=TRUE))
 
 #P12 example
+data_co2 <- read.csv2("./data_csv_files/Unit5_CO2.csv",header = T)
+CO2<-data_co2$CO2
+Machine<-data_co2$Machine
+Machine_Factor <- as.factor(Machine)
+anova_machine <- aov(lm(CO2~Machine_Factor))
+summary(anova_machine)
+require(graphics)
+plot(TukeyHSD(anova_machine,"Machine_Factor",ordered = T))
+
+
+#two factors
+data_computer<-read.csv2("./data_csv_files/Unit5_Computers.csv",header = T)
+Card <- data_computer$CARD
+Speed <- data_computer$SPEED
+Performance <- data_computer$Performance
+Card_Factor <- as.factor(Card)
+Speed_Factor <- as.factor(Speed)
+anova_two_factors <- aov(lm(Performance~Card_Factor*Speed_Factor))
+summary(anova_two_factors)
+#the two factors and the interaction are significant
+#now try to show the means plot
+#table of means
+aggregate(Performance~Card_Factor+Speed_Factor, FUN = "mean")
+#interaction plot: performance as a function of speed, for each value
+interaction.plot(Speed_Factor,Card_Factor,Performance,
+                 xlab = "Speed_Factor",
+                 ylab = "Performance",
+                 trace.label = "Card_Factor",
+                 main="Interaction Plot",
+                 col = c("blue","red","green"),
+                 bg=c("blue","red","green"),
+                 pch = c(18,24,22),
+                 type = "b")
