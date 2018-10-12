b855baae9eea72b0d2e009130befe0c9ce325f6e
Hong <hong.yu@groupe-esigelec.org>
2018-10-12 09:32:33 +0200

    add Unit6.R

---
+++ exercise_R/Unit6.R
@@ -0,0 +1,6 @@
+#Exercise about lifespan
+data_lifespan <- read.csv2("./data_csv_files/Unit6_Lifespan.csv", header = T)
+#define variable
+Lifespan <- data_lifespan$Lifespan
+Price <- data_lifespan$Price
+Use <- data_lifespan$Use
